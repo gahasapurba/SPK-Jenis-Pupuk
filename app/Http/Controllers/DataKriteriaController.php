ae4e0da3f28eaff087eb19d33271ae7c686bd3af
SuryantoRay <suryantopanjaitan44@gmail.com>
2023-03-23 08:34:44 +0700

    halaman kriteria

---
+++ app/Http/Controllers/DataKriteriaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataKriteriaController extends Controller
+{
+    // public function index()
+    // {
+    //     return view('pages.dashboard.dataKriteria.index');
+    // }
+
+    // public function addDataKriteria()
+    // {
+    //     return view('pages.dashboard.dataKriteria.addData');
+    // }
+
+    // public function editDataKriteria()
+    // {
+    //     return view('pages.dashboard.dataKriteria.editData');
+    // }
+
+    // public function ProsesTambahDataKriteria()
+    // {
+    //     return redirect()->route('indexDataKriteria')->with(['success' => 'Data Berhasil Disimpan!']);
+    // }
+
+    // public function ProsesEditDataKriteria()
+    // {
+    //     return redirect()->route('indexDataKriteria')->with(['success' => 'Data Berhasil Diupdate!']);
+    // }
+
+    // public function hapusData()
+    // {
+    //     return redirect()->route('indexDataKriteria')->with(['success' => 'Data Berhasil Dihapus!']);
+    // }
+}
