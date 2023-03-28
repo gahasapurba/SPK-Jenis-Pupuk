a89db545eaa587bbcd714c316376f302ba073b4c
Gahasa Purba <gahasapurba.tech@gmail.com>
2023-03-28 21:14:06 +0700

    kustomisasi final seluruh halaman autentikasi

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,44 +1,59 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
+@section('title')
+    Ubah Kata Sandi
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+<div class="row g-0 auth-row">
+    <div class="col-lg-6">
+        <div class="auth-cover-wrapper bg-primary-100">
+            <div class="auth-cover">
+                <div class="title text-center">
+                    <h1 class="text-primary mb-10">Ubah Kata Sandi</h1>
+                    <p class="text-medium">Silahkan masukkan email yang anda pakai dalam mendaftarkan akun anda sebelumnya. Anda akan menerima email yang berisi tautan instruksi untuk mengubah kata sandi anda</p>
+                </div>
+                <div class="cover-image">
+                    <img src="{{ asset('assets/dashboard/images/auth/reset-password.svg') }}" alt="Reset Password 1" />
+                </div>
+                <div class="shape-image">
+                    <img src="{{ asset('assets/dashboard/images/auth/shape.svg') }}" alt="Reset Password 2" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="signin-wrapper">
+            <div class="form-wrapper">
+                <h6 class="mb-15">Silahkan Ubah Kata Sandi</h6>
+                <p class="text-sm mb-25">Masukkan email anda untuk dapat mengubah kata sandi</p>
+                @if (session('status'))
+                <div class="alert-box success-alert">
+                    <div class="alert">
+                        <p class="text-medium">{{ session('status') }}</p>
+                    </div>
+                </div>
+                @endif
+                <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="input-style-1">
+                                <label for="email">Email</label>
+                                <input class="form-control @error('email') is-invalid @enderror" type="text" id="email" name="email" value="{{ old('email') }}" autofocus placeholder="Masukkan email anda" />
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+                        <div class="col-12">
+                            <div class="button-group d-flex justify-content-center flex-wrap">
+                                <button class="main-btn primary-btn btn-hover w-100 text-center" type="submit">Kirim Tautan</button>
                             </div>
                         </div>
-                    </form>
+                    </div>
+                </form>
+                <div class="d-flex justify-content-between flex-wrap pt-40">
+                    <a href="{{ route('login') }}" class="hover-underline">Masuk</a>
+                    <p>Masih belum memiliki akun? <a href="{{ route('register') }}" class="hover-underline">Buat akun</a></p>
                 </div>
             </div>
         </div>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,101 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
+@section('title')
+    Ubah Kata Sandi (Buat Kata Sandi Baru)
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
+<div class="row g-0 auth-row">
+    <div class="col-lg-6">
+        <div class="auth-cover-wrapper bg-primary-100">
+            <div class="auth-cover">
+                <div class="title text-center">
+                    <h1 class="text-primary mb-10">Buat Kata Sandi Baru</h1>
+                    <p class="text-medium">Silahkan buat kata sandi baru untuk akun anda untuk melanjutkan</p>
+                </div>
+                <div class="cover-image">
+                    <img src="{{ asset('assets/dashboard/images/auth/reset-password.svg') }}" alt="Reset Password 1" />
+                </div>
+                <div class="shape-image">
+                    <img src="{{ asset('assets/dashboard/images/auth/shape.svg') }}" alt="Reset Password 2" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="signup-wrapper">
+            <div class="form-wrapper">
+                <h6 class="mb-15">Silahkan Buat Kata Sandi Baru</h6>
+                <p class="text-sm mb-25">Buat kata sandi baru yang menurut anda kuat dan mudah diingat untuk akun anda</p>
+                <form method="POST" action="{{ route('password.update') }}">
+                @csrf
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="input-style-1">
+                                <label for="email">Email</label>
+                                <input class="form-control @error('email') is-invalid @enderror" type="text" id="email" name="email" value="{{ $email ?? old('email') }}" placeholder="Masukkan email anda" />
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                        <div class="col-12">
+                            <div class="input-style-1">
+                                <label for="password">Kata Sandi</label>
+                                <input class="form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" placeholder="Masukkan kata sandi anda" />
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <div class="col-12">
+                            <div class="form-check checkbox-style mb-30">
+                                <input class="form-check-input" type="checkbox" id="show_password" name="show_password" onclick="myFunction1()" />
+                                <label class="form-check-label" for="show_password">Tampilkan kata sandi</label>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
+                        <div class="col-12">
+                            <div class="input-style-1">
+                                <label for="password-confirm">Konfirmasi Kata Sandi</label>
+                                <input class="form-control @error('password') is-invalid @enderror" type="password" id="password-confirm" name="password_confirmation" placeholder="Masukkan ulang kata sandi anda" />
                             </div>
                         </div>
-                    </form>
+                        <div class="col-12">
+                            <div class="form-check checkbox-style mb-30">
+                                <input class="form-check-input" type="checkbox" id="show_password_confirmation" name="show_password_confirmation" onclick="myFunction2()" />
+                                <label class="form-check-label" for="show_password_confirmation">Tampilkan konfirmasi kata sandi</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="button-group d-flex justify-content-center flex-wrap">
+                                <button class="main-btn primary-btn btn-hover w-100 text-center" type="submit">Ubah Kata Sandi</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="singup-option pt-40">
+                    <p class="text-sm text-medium text-dark text-center">Sudah mengingat kembali kata sandi anda? <a href="{{ route('login') }}">Silahkan masuk</a></p>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+@push('addon-script')
+<script>
+    function myFunction1() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+    function myFunction2() {
+        var x = document.getElementById("password-confirm");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
+@endpush
